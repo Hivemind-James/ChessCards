4786ccb385b3c1693cac874ae6bf54f65836984e
jlloyddominik <101663792@student.swin.edu.au>
2018-09-29 17:19:40 +1000

    added some of the base code for the game controller class

--- src/Game.cs
+++ src/Game.cs
@@ -8,5 +8,43 @@ namespace MyGame
 {
     public class Game
     {
+        private Dictionary<PlayerColour, Player> _players;
+        private Board _board;
+        private int _turn;
+        private GameState _state;
+
+        public Game()
+        {
+            _players = new Dictionary<PlayerColour, Player>();
+            foreach (PlayerColour player in Enum.GetValues(typeof(PlayerColour)))
+            {
+                //_players.Add(player, new Player(player));
+            }
+
+            _board = new Board();
+            //_board.Setup();
+
+            _turn = 1;
+
+            _state = GameState.Setup;
+        }
+
+        public void DoMove()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void PlayCard()
+        {
+            throw new NotImplementedException();
+        }
+
+        public PlayerColour ActivePlayer
+        {
+            get
+            {
+                return (_turn % 2 == 1) ? PlayerColour.White : PlayerColour.Black;
+            }
+        }
     }
 }
