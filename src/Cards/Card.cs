0c69a0f3e768675fe5af9e5a6febdef096c3e51b
jlloyddominik <101663792@student.swin.edu.au>
2018-10-07 11:09:44 +1100

    added cards

--- src/Cards/Card.cs
+++ src/Cards/Card.cs
@@ -30,6 +30,14 @@ namespace MyGame
                 return _minTurn;
             }
         }
+
+        public PlayerColour Owner
+        {
+            get
+            {
+                return _owner;
+            }
+        }
     }
 
     public class KillPiece : Card
@@ -71,4 +79,100 @@ namespace MyGame
             }
         }
     }
+
+    public class Promote : Card
+    {
+        private List<Kind> _targets;
+        private List<Kind> _promoOptions;
+
+        public Promote (int turn, PlayerColour owner, List<Kind> targets, List<Kind> promotionOptions) : base(turn, owner)
+        {
+            _targets = targets;
+            _promoOptions = promotionOptions;
+        }
+
+        public override void DrawLarge()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void DrawSmall()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool IsPlayable(Game game)
+        {
+            bool isPlayable = false;
+            foreach (Kind target in _targets)
+            {
+                isPlayable = game.Board.Contains(target, Owner) && game.Turn >= MinimumTurn;
+                if (isPlayable) break;
+            }
+            return isPlayable;
+        }
+
+        public override void Resolve(Game game)
+        {
+            //if (_promoOptions.Contains(Kind.Queen) && game.Board.Contains(Kind.Queen, Owner)); //Player can't choose queen
+            throw new NotImplementedException();
+        }
+    }
+
+    public class Demote : Card
+    {
+        private List<Kind> _targets;
+        private List<Kind> _promoOptions;
+        private PlayerColour _enemy;
+
+
+        public Demote(int turn, PlayerColour owner, List<Kind> targets, List<Kind> promotionOptions) : base(turn, owner)
+        {
+            _targets = targets;
+            _promoOptions = promotionOptions;
+        }
+
+        public override void DrawLarge()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void DrawSmall()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool IsPlayable(Game game)
+        {
+            bool isPlayable = false;
+            foreach (Kind target in _targets)
+            {
+                isPlayable = game.Board.Contains(target, _enemy) && game.Turn >= MinimumTurn;
+                if (isPlayable) break;
+            }
+            return isPlayable;
+        }
+
+        public override void Resolve(Game game)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    /*******************************************************************************
+     *  CARDS TO ADD
+     * *****************************************************************************
+     * 
+     *  Killer Queen    : kills all friendly and enemy pieces around the queen
+     *  Fortify         : builds an untakeable blocking piece next to a rook you control
+     *  Kagemusha       : allows you to move your king 1 space (or no spaces) then creates a body double that looks and acts like a king piece
+     *  Four Horsemen   : promotes a number of units until you have 4 knights
+     *  ???             : promotes all your non-king pieces to body doubles
+     *  Castle          : a card each player gets by default, it functions like the castle maneuver from normal chess
+     *  Sidestep        : allows you to move any one piece 1 space before you make your move (or at the end of turn for balance if we can make that work)
+     *  Matricide       : changes the rules so that the queen is the piece that needs to be taken in order to win
+     *  Divine intervention
+     *                  : switches the position of any piece with a bishop you control (also might need to be at the end of turn for balance)
+     * 
+     */
 }
